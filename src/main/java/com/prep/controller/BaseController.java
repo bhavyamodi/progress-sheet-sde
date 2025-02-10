870457226d4314cceebb49d3ab3905912878bcd2
bhavyamodi <bhavya.modi@ticgroup.com.au>
2025-02-10 12:55:22 +0530

    base response

--- src/main/java/com/prep/controller/BaseController.java
+++ src/main/java/com/prep/controller/BaseController.java
@@ -1,4 +1,15 @@
 package com.prep.controller;
 
+import com.prep.dto.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public class BaseController {
+    protected <T> ResponseEntity<ApiResponse<T>> successResponse(String message, T data) {
+        return ResponseEntity.ok(new ApiResponse<>(HttpStatus.OK, message, data));
+    }
+
+    protected <T> ResponseEntity<ApiResponse<T>> errorResponse(HttpStatus status, String message) {
+        return ResponseEntity.status(status).body(new ApiResponse<>(HttpStatus.INTERNAL_SERVER_ERROR, message, null));
+    }
 }
