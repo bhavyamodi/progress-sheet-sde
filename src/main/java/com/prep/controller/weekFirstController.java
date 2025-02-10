784cf46bdaeffdcc54dcd0e355b9ff2e981e6dba
bhavyamodi <bhavya.modi@ticgroup.com.au>
2025-02-10 13:06:33 +0530

    testing

--- src/main/java/com/prep/controller/weekFirstController.java
+++ src/main/java/com/prep/controller/weekFirstController.java
@@ -1,7 +1,13 @@
 package com.prep.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class weekFirstController extends BaseController{
+    @GetMapping("/auth")
+    public boolean checkExistingAPI() {
+        return true;
+    }
 }
