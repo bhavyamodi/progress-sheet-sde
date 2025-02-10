513fc0873d48e1d2420f9452581f25f8fcdfa147
bhavyamodi <bhavya.modi@ticgroup.com.au>
2025-02-10 10:37:10 +0530

    initial commit

---
+++ src/main/java/com/prep/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.prep.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springdoc.core.models.GroupedOpenApi;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**") // Customize paths as needed
+                .build();
+    }
+}
